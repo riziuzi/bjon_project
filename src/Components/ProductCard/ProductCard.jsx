df0f47d378792b90c846bcfe83a7c087171f8217
rishi <rishi18december@gmail.com>
2024-01-26 00:22:35 +0530

    Added some corrected files

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
-export default function ProductCard({data}) {
-    console.log(data)
+export default function ProductCard({ data }) {
     return (
         <>
-            <div className="product flex   my-3">
+            <Helmet>
+                <title>{`${data.title} - Your Website Title`}</title>
+
+                <meta name="description" content={`Explore details about ${data.title}. ${data.description}`} />
+                <meta name="keywords" content={`${data.title}, ${data.description}, in stock, price, discount`} />
+                <meta name="robots" content="index, follow" />
+
+                <meta property="og:title" content={`${data.title} - Your Website Title`} />
+                <meta property="og:description" content={`Explore details about ${data.title}. ${data.description}`} />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={`https://www.yourwebsite.com/datas/`} />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={`${data.title} - Your Website Title`} />
+                <meta name="twitter:description" content={`Explore details about ${data.title}. ${data.description}`} />
+            </Helmet>
+            <div className="Product flex   my-3">
                 <img src="./img/1.png" alt="img" className=" w-3/5 object-cover shadow-md rounded-l-3xl -mr-5" />
                 <div className="discription flex flex-col bg-skin-bg300 w-2/5 shadow-md  px-5 rounded-3xl">
-                    <div className="title font-bold font-sans text-lg my-3 text-left">Nike React Miler</div>
-                    <div className="about font-sans mb-3 text-sm">Men's road running shoes</div>
+                    <div className="title font-bold font-sans text-lg my-3 text-left">{data.title}</div>
+                    <div className="about font-sans mb-3 text-sm">{data.description}</div>
                     <div className="availability text-sm w-1/2 text-skin-text100 mt-16">Available</div>
                     <div className="dummy flex text-sm my-2 items-center mt-3 ">
                         <div className="Left text-sm bg-skin-primary200 py-1 px-1 rounded-3xl w-1/2 text-skin-text100">
